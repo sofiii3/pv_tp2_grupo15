935200ccf53be65f347a548ac9701d45c5f23f24
sofiii3 <cabanasofia440@gmail.com>
2025-04-17 04:09:20 -0300

    Desarrollo del ejercicio4 completado

---
+++ js/ejercicio4.js
@@ -0,0 +1,7 @@
+const lado = 5;
+
+const calcularArea = (l) => 2 * l;
+const calcularPerimetro = (l) => l + l + l + l;
+
+console.log("Área del cuadrado:", calcularArea(lado));
+console.log("Perímetro del cuadrado:", calcularPerimetro(lado));
